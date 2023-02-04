b34e4c0fc0b1983a4d5f692e03e4a4939f6bb3fd
Ihsanfati <ifat.ichan@gmail.com>
2023-02-04 20:47:40 +0700

    Early Stage

---
+++ src/components/main.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import TextBox from "./texField";
+import axios from 'axios';
+import swal from 'sweetalert';
+import './main.css';
+
+function Main(){
+    const [prediction, setPrediction] = useState('');
+
+    const onChangePrediction = (e) => {
+        setPrediction(e.target.value);
+    }
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const result = {
+            prediction: prediction
+        }
+        console.log(result)
+        axios.post('/predict', result)
+            .then((res) => {
+                console.log(res.data);
+                if(res.data === "Pretexting") {
+                    swal({
+                        title: "Pretexting!",
+                        text: "Kalimat yang Anda masukkan merupakan " + res.data + " !!",
+                        icon: "error",
+                        button: false,
+                        timer: 4000
+                    });
+                    setPrediction('');
+                } else {
+                    swal({
+                        title: "Not Pretexting!",
+                        text: "Kalimat yang Anda masukkan bukan Pretexting",
+                        icon: "success",
+                        button: false,
+                        timer: 4000
+                    });
+                    setPrediction('');
+                }
+            })
+            .catch(error => {
+                console.log(error.response)
+            })
+    }
+
+    return(
+        <div className='card'>
+            <Card className='cardStyle'>
+                <CardContent>
+                    <div className='pretexting-detector' style={{marginBottom: 20}}>Pretexting Detector</div>
+                    <TextBox label="Masukkan teks"
+                        value = {prediction}
+                        onChange={onChangePrediction}
+                    />
+                </CardContent>
+                <CardActions className='CardActions'>
+                    <Button 
+                        style={{
+                            background: 'grey', color: 'white', marginBottom: 20
+                        }}
+                        onClick={onSubmit}>Check !!</Button>
+                </CardActions>
+            </Card>
+        </div>
+    )
+}
+
+export default Main;
